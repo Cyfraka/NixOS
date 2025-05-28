e6ef5cd0c5b61b76d6142bc7f7ec2cb05a2350f2
Cyfraka <cyfraka@protonmail.com>
2025-05-28 23:46:06 +0200

    changes

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Cyfraka NixOS and Home Manager configuration using unstable nixpkgs";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/master"; # This is the unstable channel
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -13,15 +13,19 @@
       pkgs = import nixpkgs { inherit system; };
     in {
       nixosConfigurations = {
-        ThinkPad-X1-Carbon = nixpkgs.lib.nixosSystem {
+        x1 = nixpkgs.lib.nixosSystem {
           inherit system;
-          modules = [ ./configuration.nix ];
+          modules = [ ./hosts/x1/configuration.nix ];
+        };
+        x230 = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [ ./hosts/x230/configuration.nix ];
         };
       };
       homeConfigurations = {
         cyfraka = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
-          modules = [ ./home.nix ];
+          modules = [ ./modules/home.nix ];
         };
       };
     };
